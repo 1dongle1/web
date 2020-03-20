5275c4f4e1fca7ecc6d94c6ddd97e794d4df3eb0
“ <824431016@qq.com>
2020-03-20 21:58:59 +0800

    web2

--- app.js
+++ app.js
@@ -1,7 +1,71 @@
+// const express = require('express')
+// const fs = require('fs')
+// const app = express()
+// const port = 3000
+// app.use(express.json())
+
+// app.get('/accounts', (req, res) => fs.readFile('./data.json','utf-8',(err, data )=>{
+//     if (err) {
+//         res.status(500).send()
+//     }
+//     else{
+//         res.json(JSON.parse(data))
+//     }
+// }))
+
+// asyncReadFile =function(path)
+// {
+//     return new Promise(
+//         function(resolve,reject){
+//             fs.readFile(path,'utf-8',function(err,data){
+//                 if(err){
+//                     reject(err)
+//                 }
+//                 resolve(data)
+//             })
+//         }
+//         ).catch(err=>{
+//             return err
+//         })
+// }
+// const asyncWriteFile =function (string,path){
+//     return new Promise(function(resolve,reject){
+//         fs.writeFile(path,string,function(err){
+//             if(err){
+//                 reject(err)
+//             }
+//             resolve(data)
+//         })
+//     }).catch(err=>{
+//         return err
+//     })
+
+// }
+// const createAccount = async (req,res) =>{
+//     const newAccount =req.body
+//     const file = await asyncReadFile('./data.json') 
+//     const account = JSON.parse(file)
+//     if(account.filter(v=>v.email===newAccount.email).length!=0){
+//         res.status(400).send
+//     }
+//     else{
+//         account.push(newAccount)
+//         await asyncReadFile('./data.json',JSON.stringify(account))
+//         await asyncWriteFile(JSON.stringify(account),'./data.json')
+//         res.status(201).send(accounts)
+//     }
+
+// }
+
+// app.post('/accounts', createAccount)
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// exports.app =app
 const express = require('express')
 const fs = require('fs')
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.get('/accounts', (req, res) => fs.readFile('./data.json','utf-8',(err, data )=>{
     if (err) {
@@ -12,5 +76,49 @@ app.get('/accounts', (req, res) => fs.readFile('./data.json','utf-8',(err, data 
     }
 }))
 
+asyncReadFile =function(path)
+{
+    return new Promise(
+        function(resolve,reject){
+            fs.readFile(path,'utf-8',function(err,data){
+                if(err){
+                    reject(err)
+                }
+                resolve(data)
+            })
+        }
+        )
+    }
+const asyncWriteFile =function (string,path){
+    return new Promise(function(resolve,reject){
+        fs.writeFile(path,string,function(err,data){
+            if(err){
+                reject(err)
+            }
+            resolve(data)
+        })
+    }).catch(err=>{
+        return err
+    })
+
+}
+const createAccount = async (req,res) =>{
+    const newAccount =req.body
+    const file = await asyncReadFile('./data.json') 
+    const account = JSON.parse(file)
+    if(account.filter(v=>v.email===newAccount.email).length!=0){
+        res.status(400).send
+    }
+    else{
+        account.push(newAccount)
+        await asyncReadFile('./data.json',JSON.stringify(account))
+        await asyncWriteFile(JSON.stringify(account),'./data.json')
+        res.status(201).send(account)
+    }
+
+}
+
+app.post('/accounts', createAccount)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+exports.app =app
